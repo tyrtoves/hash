a00be841a07f8af62ad2461ef94ca7385d67478a
Evgeniy <evgeniy@debian>
2014-04-25 22:33:11 +0400

    second commit

--- hash.c
+++ hash.c
@@ -1,4 +1,4 @@
-#include"../gmp-6.0.0/gmp.h"
+#include<gmp.h>
 #include<stdio.h>
 #include<pthread.h>
 #include<fcntl.h>
@@ -13,44 +13,50 @@ mpz_t A;
 
 void* module(void *arg) {
 		
-	int limit = 1000 * (*(int *)arg + 1);
-	int i = (*(int *)arg) * 1000;
+	mpz_t limit; 
 	mpz_t rop;
-	mpz_realloc2(rop, 1000);
+	mpz_t i;
+	
+	mpz_init_set_ui(limit, 1000 * (*(int *)arg + 1));
+	
+	mpz_init_set_ui(i, (*(int *)arg) * 1000);
+	
 	while(1) {
-		while(i <= limit) {
-			
-			mpz_powm_ui(rop, g, i, p);
+		while( mpz_cmp(limit, i) ) {
+		
+			mpz_add_ui(i, i, 1);
+			mpz_powm(rop, g, i, p);
 			
 			if ( !mpz_cmp(A, rop) ) {
-				printf("------ Result = %d ------\n", i);
-				mpz_clears(g, p, A);
+				printf("------ Result =  ");
+				mpz_out_str(stdout, 10, i);
+				printf("\n");
 				exit(0);
 			}
-			i++;
+		
 		}
-		limit = limit + 4000;
+		mpz_add_ui(i, i, 4000);
 	}
 	pthread_exit(NULL);
 }
 
 int main(int argc, char** argv) { 
  	
-    mpz_init_set_str (g, argv[1], 10);
-    mpz_init_set_str (p, argv[2], 10);
-    mpz_init_set_str (A, argv[3], 10);
+    mpz_set_str (g, argv[1], 10);
+    mpz_set_str (p, argv[2], 10);
+    mpz_set_str (A, argv[3], 10);
 	
     int i, n;
     n = 4; 
     pthread_t pthreads[n];
     for (i = 0; i < n; ++i ) {
     	
-    	pthread_create(pthreads + i, NULL, &module, &i);
+    	pthread_create(i + pthreads, NULL, &module, &i);
     }
-    void* value;
+ 	void* res   ;
     for (i = 0; i < n; i++) {
-   		pthread_join(pthreads[i] , value);
+   		pthread_join(pthreads[i], res);
   	}
-		
+	mpz_clears(g, p, A);
 	return 0;
 }
