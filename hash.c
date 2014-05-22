6b3379174e8bc3fcdd80a0123bcba1e64d101d57
Evgeniy <evgeniy@debian>
2014-05-22 20:57:06 +0400

    second commit

--- hash.c
+++ hash.c
@@ -12,45 +12,31 @@ mpz_t p;
 mpz_t A;
 
 void* module(void *arg) {
-		
-	mpz_t limit; 
 	mpz_t rop;
 	mpz_t i;
 	
-	mpz_init_set_ui(limit, 1000 * (*(int *)arg + 1));
+	mpz_init_set_ui(i, 0);
+	mpz_init_set_ui(rop, 0);
 	
-	mpz_init_set_ui(i, (*(int *)arg) * 1000);
-	
-	while(1) {
-		while( mpz_cmp(limit, i) ) {
-		
-			mpz_add_ui(i, i, 1);
-			mpz_powm(rop, g, i, p);
-			
-			if ( !mpz_cmp(A, rop) ) {
-				printf("------ Result =  ");
-				mpz_out_str(stdout, 10, i);
-				printf("\n");
-				exit(0);
-			}
-		
-		}
-		mpz_add_ui(i, i, 4000);
+	while (mpz_cmp(A, rop)) {
+		mpz_powm(rop, g, i, p);
+		mpz_add_ui(i, i, (*(int *)arg + 1));
 	}
-	pthread_exit(NULL);
+	mpz_sub_ui(i, i, (*(int *)arg + 1));
+	printf("------ Result =  ");
+	mpz_out_str(stdout, 10, i);
+	printf("\n");
+	exit(0);
 }
 
 int main(int argc, char** argv) { 
- 	
     mpz_set_str (g, argv[1], 10);
     mpz_set_str (p, argv[2], 10);
-    mpz_set_str (A, argv[3], 10);
-	
+    mpz_set_str (A, argv[3], 10);	
     int i, n;
     n = 4; 
     pthread_t pthreads[n];
     for (i = 0; i < n; ++i ) {
-    	
     	pthread_create(i + pthreads, NULL, &module, &i);
     }
  	void* res   ;
